62a92e11ecff6aba604763d3c2d93e4fc234b4b5
Sathya narrayanan <sourcepirate@Sathyas-MacBook-Pro.local>
2022-08-29 02:25:41 +0800

    updated dependencies and redis celery template

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,10 +6,11 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
-
+    
+has_celery = '{{ cookiecutter.need_celery }}'
 
 if __name__ == '__main__':
 
-    if '{{ cookiecutter.need_celery }}':
+    if  not has_celery:
         remove_file('config/worker.py')
         remove_file('Dockerfile.worker')
